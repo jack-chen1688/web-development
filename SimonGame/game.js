b291524030f3ef8e7e454525327bd41adfd6651f
xuehua <xuehua@gmail.com>
2020-10-31 22:44:27 -0700

    Use .fadeOut(100).fadeIn(100) to animate a button is selected.

--- SimonGame/game.js
+++ SimonGame/game.js
@@ -16,7 +16,8 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
-  buttonPress($("#" + randomChosenColor));
+  //buttonPress($("#" + randomChosenColor));
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
   makeSound(randomChosenColor);
 }
 
