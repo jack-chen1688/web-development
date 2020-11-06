2d08d51009546b75e35046f33deb568728ac1db8
Xuehua Chen <xuehua@gmail.com>
2020-11-06 09:45:19 -0800

    Implementation of a simple calculator web app.

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req, res) {
-  res.send("Hello World!");
-})
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  console.log(req.body);
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1 + num2;
+  res.send("Result is " + result);
+});
+
 app.listen(3000, function(){
   console.log("Wait at port 3000");
-})
+});
