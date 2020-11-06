b9b28a94b0d66000893329bbfba834d5d6d55347
Xuehua Chen <xuehua@gmail.com>
2020-11-05 19:36:51 -0800

    Added more routes to different url addresses.

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -5,6 +5,17 @@ app.get("/", function(req, res) {
   res.send("<h1>Hello World!</h1>");
 });
 
+app.get("/contact", function(req, res) {
+  res.send("Contact me at xuehua@gmail.com.");
+});
+
+app.get("/about", function(req, res) {
+  res.send("This is my express test page.");
+});
+
+app.get("/hobbies", function(req, res) {
+  res.send("<ul><li>Skating</li><li>Go</li></ul>")
+})
 app.listen(3000, function() {
   console.log("Listen at port 3000.");
 });
