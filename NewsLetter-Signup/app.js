7d6a72c13c3bb27fd1bb75fef02b30b23746a3db
Xuehua Chen <xuehua@gmail.com>
2020-11-14 16:38:28 -0800

    Enable bootstrap support and enable static serving for the NewsLetter project.

--- NewsLetter-Signup/app.js
+++ NewsLetter-Signup/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request")
+
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("."));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html");
