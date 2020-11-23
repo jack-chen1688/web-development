efda699c5e9ec304a9412c7e8dbca456a8fefd15
Xuehua Chen <xuehua@gmail.com>
2020-11-22 16:29:00 -0800

    Added App, Header, Note and Footer.

--- keeper-app/src/App.jsx
+++ keeper-app/src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
 
 function App() {
     return (
-        <h1>Keeper App</h1>
+        <div>
+            <Header />
+            <Note />
+            <Footer />
+        </div>
     );
 }
 
