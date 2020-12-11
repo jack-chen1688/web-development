addaf09f22f17bba7ce2757e77439f001c27b248
Xuehua Chen <xuehua@gmail.com>
2020-12-10 21:35:24 -0800

    Some minor changes.

--- NewsLetter-Signup/app.js
+++ NewsLetter-Signup/app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }))
 
-app.use(express.static("."));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
