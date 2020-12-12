13bb439a81c4a0b19fe5487c0decce8a6ce35092
Xuehua Chen <xuehua@gmail.com>
2020-12-11 23:14:37 -0800

    Added post support.

--- NewsLetter-Signup/app.js
+++ NewsLetter-Signup/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request")
+const request = require("request");
+const https = require("https");
 
-const app = express()
+const app = express();
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -15,11 +16,44 @@ app.get("/", function(req, res) {
 })
 
 app.post("/", function(req, res) {
-    console.log(req.body.firstName)
-    console.log(req.body.lastName)
-    console.log(req.body.email)
-})
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        'email_address': email,
+        'status':'subscribed',
+        'merge_fields': {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  }
+  const jsonData = JSON.stringify(data);
+  const url = "https://us2.api.mailchimp.com/3.0/lists/bb83853d04";
+  const options = {
+    method: "POST",
+    auth: 'xuehua1:4e674b673a55a7cbba5cd9e37e4486d9-us2',
+  }
+
+  req = https.request(url, options, function(response) {
+    response.on('data', function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  req.write(jsonData);
+  req.end();
+});
 
 app.listen(3000, function() {
   console.log("Listening at port 3000.")
-})
+});
+
+// api key
+// 4e674b673a55a7cbba5cd9e37e4486d9-us2
+// list id/audience id
+// bb83853d04
