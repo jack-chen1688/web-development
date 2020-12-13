33b4df7bac612956391ff5357164ef059fd3f3f0
Xuehua Chen <xuehua@gmail.com>
2020-12-13 15:40:46 -0800

    use process.env.PORT for heroku environment.

--- NewsLetter-Signup/app.js
+++ NewsLetter-Signup/app.js
@@ -56,7 +56,7 @@ app.post("/", function(req, res) {
   req.end();
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Listening at port 3000.")
 });
 
