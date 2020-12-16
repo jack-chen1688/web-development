767f3d68e998753a3c06c35141dbc8574f5fdec1
Xuehua Chen <xuehua@gmail.com>
2020-12-16 11:21:39 -0800

    More tests on updateOne and deleteOne.

--- FruitMongoose/app.js
+++ FruitMongoose/app.js
@@ -3,13 +3,30 @@ mongoose.connect('mongodb://localhost:27017/fruitsDB',
   {useNewUrlParser: true, useUnifiedTopology: true});
 
 const fruitSchema = new mongoose.Schema({
-  name: String,
-  rating: Number,
+  name: {
+    type: String,
+    required: [true, "Please check your data entry, no name is specified."]
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10
+  },
   review: String
 });
 
 //Automatically create a collection of "fruits"
 const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const peach = new Fruit({
+  name: "Peach",
+  rating: 11,
+  review: "Peaches are so yummy"
+});
+
+peach.save(function(err) {
+  console.log(err);
+});
 
 const apple = new Fruit({
   name: "Apple",
@@ -17,7 +34,7 @@ const apple = new Fruit({
   review: "Pretty solid as a fruit"
 });
 
-apple.save();
+// apple.save();
 
 const kiwi = new Fruit({
   name: "Kiwi",
@@ -35,12 +52,62 @@ const banana = new Fruit({
   name: "Banana",
   rating: 9,
   review: "Healthy food"
-})
+});
 
-Fruit.insertMany([kiwi, orange, banana], function(err) {
+const mango = new Fruit({
+  name: "Mango",
+  rating: 9,
+  review: "Mango is healthy"
+});
+
+mango.save();
+// Fruit.insertMany([kiwi, orange, banana], function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully inserted all the fruits into fruitsDB");
+//   }
+// })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFruit: fruitSchema
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: "Amy",
+  age: 32,
+  favoriteFruit: mango
+});
+
+person.save();
+
+// person.save();
+
+// Person.updateOne({name: "John"}, {age: 34}, function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Updated successfully");
+//   }
+// })
+
+Person.deleteOne({_id: "5fd994cc6143d85e54c3a370"}, function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Deleted successfully")
+    }
+});
+
+Fruit.find(function(err, fruits) {
   if (err) {
     console.log(err);
   } else {
-    console.log("Successfully inserted all the fruits into fruitsDB");
+    mongoose.connection.close();
+    fruits.forEach(fruit => console.log(fruit.name));
   }
-})
+});
